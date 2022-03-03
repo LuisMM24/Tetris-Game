404907ea9aba6e7f411c372ca0cfdb21bf79f859
Luis Molina <luismateo2416@gmail.com>
2022-03-03 11:05:44 +0100

    skeleton tetris

---
+++ src/components/Playground/Block/Block.jsx
@@ -0,0 +1,11 @@
+
+import './block.scss'
+export default function Block () {
+  const blockTypes = ['L', 'J', 'I', '0', 'S', 'T', 'Z']
+  console.log(blockTypes)
+  return (
+    <div className='box'>
+      <div />
+    </div>
+  )
+}
---
+++ src/components/Playground/Playground.jsx
@@ -0,0 +1,11 @@
+import Block from './Block'
+import './playground.scss'
+export default function Playground () {
+  return (
+    <main>
+      <div className='playgroundContainer'>
+        <Block />
+      </div>
+    </main>
+  )
+}
